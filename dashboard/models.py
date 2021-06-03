dd8d8b4d57900f2c3454beaf828050b15be9fbca
jinho <g1063114@naver.com>
2021-06-03 17:59:57 +0900

    create user class

--- dashboard/models.py
+++ dashboard/models.py
@@ -6,4 +6,4 @@ class User(db.Model):
     password = db.Column(db.String(200),nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     univ = db.Column(db.String(100), nullable=False)
-    dept = db.Column(db.String(150), nullable=False)
+    dept = db.Column(db.String(120), nullable=False)
