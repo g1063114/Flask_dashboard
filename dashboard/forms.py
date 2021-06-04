fb2d0d042336edc591f9ffe11b91ad84c291b451
jinho <g1063114@naver.com>
2021-06-04 17:17:29 +0900

    login,logout, reset password test...

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -11,3 +11,14 @@ class UserCreateForm(FlaskForm):
     email = EmailField('이메일',validators=[DataRequired('이메일 입력은 필수 항목입니다.'),Email()])
     univ = StringField('소속 대학교',validators=[DataRequired('소속 대학교 입력은 필수 항목입니다.'),Length(min=3,max=20)])
     dept = StringField('소속 부서',validators=[DataRequired('소속 부서 입력은 필수 항목입니다.')])
+
+class UserLoginForm(FlaskForm):
+    userid = StringField('사용자아이디',validators=[DataRequired('아이디 입력은 필수 항목입니다.'),Length(min=3,max=25)])
+    password = PasswordField('비밀번호',validators=[DataRequired()])
+
+class UserCheckForm(FlaskForm):
+    userid = StringField('사용자아이디',validators=[DataRequired('아이디 입력은 필수 항목입니다.'),Length(min=3,max=25)])
+
+class UserResetPasswordForm(FlaskForm):
+    password = PasswordField('새로운 비밀번호',validators=[DataRequired('비밀번호 입력은 필수 항목입니다.'),EqualTo('password2','비밀번호가 일치하지 않습니다.')])
+    password2 = PasswordField('비밀번호 확인',validators=[DataRequired('비밀번호 확인 입력은 필수 항목입니다.')])
--- dashboard/views/auth_views.py
+++ dashboard/views/auth_views.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, url_for, render_template, flash, request
-from werkzeug.security import generate_password_hash
+from flask import Blueprint, url_for, render_template, flash, request, session, g
+from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
 
 from dashboard import db
-from dashboard.forms import UserCreateForm
+from dashboard.forms import UserCreateForm, UserLoginForm, UserResetPasswordForm, UserCheckForm
 from dashboard.models import User
 
 bp = Blueprint('auth',__name__,url_prefix='/auth')
@@ -22,3 +22,52 @@ def signup():
         else:
             flash('이미 존재하는 사용자입니다.')
     return render_template('auth/signup.html',form=form)
+
+@bp.route('/login/',methods=('GET','POST'))
+def login():
+    form = UserLoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        error = None
+        user = User.query.filter_by(userid=form.userid.data).first()
+        if not user:
+            error = '존재하지 않는 사용자입니다.'
+        elif not check_password_hash(user.password,form.password.data):
+            error = '비밀번호가 일치하지 않습니다.'
+        if error is None:
+            session.clear()
+            session['user_id'] = user.key
+            print(session)
+            return redirect(url_for('main.start_app'))
+        flash(error)
+    return render_template('auth/login.html',form=form)
+
+@bp.route('/logout/')
+def logout():
+    session.clear()
+    return redirect(url_for('main.start_app'))
+
+@bp.before_app_request
+def get_login_user():
+    user_id = session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.get(user_id)
+
+@bp.route('/check/',methods=('GET','POST'))
+def check():
+    form = UserCheckForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        user = User.query.filter_by(userid=form.userid.data).first()
+        print(user)
+        if not user:
+            flash('존재하지 않는 사용자입니다.')
+        else:
+            return redirect(url_for('auth.reset'))
+    return render_template('auth/check.html',form=form)
+
+@bp.route('/reset/',methods=('GET','POST'))
+def reset():
+    form = UserResetPasswordForm()
+    if request.method == 'POST' and form.validate_on_submit():
+
