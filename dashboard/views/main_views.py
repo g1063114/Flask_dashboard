9ba5ca39a6bc7014b0a48a3b8bc87055fdc2eebf
jinho <g1063114@naver.com>
2021-06-10 17:59:51 +0900

    chart data test

--- dashboard/views/main_views.py
+++ dashboard/views/main_views.py
@@ -8,11 +8,12 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import io
 
 bp = Blueprint('main',__name__,url_prefix='/')
-pay = pd.read_csv('C:\\projects\\Flask_dashboard\\dashboard\\data\\등록금현황.csv', encoding='cp949')
-nstu = pd.read_csv('C:\\projects\\Flask_dashboard\\dashboard\\data\\신입생충원현황.csv', encoding='cp949')
-fail = pd.read_csv('C:\\projects\\Flask_dashboard\\dashboard\\data\\중도탈락현황.csv', encoding='cp949')
-emp = pd.read_csv('C:\\projects\\Flask_dashboard\\dashboard\\data\\취업현황.csv', encoding='cp949')
 
+pay = pd.read_csv('C:\\projects\\Flask_dashboard\\dashboard\\data\\등록금현황.csv', encoding='cp949')
+student = pd.read_csv('C:\\projects\\Flask_dashboard\\dashboard\\data\\신입생충원현황.csv', encoding='cp949')
+drop_out = pd.read_csv('C:\\projects\\Flask_dashboard\\dashboard\\data\\중도탈락현황.csv', encoding='cp949')
+job = pd.read_csv('C:\\projects\\Flask_dashboard\\dashboard\\data\\취업현황.csv', encoding='cp949')
+enter_type = pd.read_csv('C:\\projects\\Flask_dashboard\\dashboard\\data\\입학유형현황.csv', encoding='cp949')
 
 @bp.route('/')
 def start_app():
@@ -28,7 +29,7 @@ def export():
 
 @bp.route('/dash1',methods=['POST'])
 def dash1():
-    temp = pd.merge(nstu,emp, on=['info_yyyy','univ_id'],how ='inner').fillna(0)
+    temp = pd.merge(student,job, on=['info_yyyy','univ_id'],how ='inner').fillna(0)
     merge_data = pd.merge(temp,pay, on=['info_yyyy','univ_id'],how ='inner').fillna(0)
 
     recruit = request.form['recruit']
@@ -53,9 +54,9 @@ def dash1():
 
 @bp.route('/dash2',methods=['POST'])
 def dash2():
-    temp2 = pd.merge(nstu,emp, on=['info_yyyy','univ_id'],how ='inner').fillna(0)
+    temp2 = pd.merge(student,job, on=['info_yyyy','univ_id'],how ='inner').fillna(0)
     temp3 = pd.merge(temp2,pay, on=['info_yyyy','univ_id'],how ='inner').fillna(0)
-    merge_data = pd.merge(temp3,fail, on=['info_yyyy','univ_id'],how ='inner').fillna(0)
+    merge_data = pd.merge(temp3,drop_out, on=['info_yyyy','univ_id'],how ='inner').fillna(0)
 
     recruit = request.form['recruit']
     employ = request.form['emp']
@@ -81,19 +82,28 @@ def dash2():
 @bp.route('/chart',methods=['POST'])
 def chart():
     year = [2012,2013,2014,2015,2016,2017,2018,2019,2020]
-    partici = list(nstu.groupby('info_yyyy').sum()['지원자'].values)
-    max_enroll = list(nstu.groupby('info_yyyy').sum()['입학정원'].values)
-    fail_values = list(fail.groupby('info_yyyy').sum()['중도탈락'].values)
-    not_enroll = list(fail.groupby('info_yyyy').sum()['미등록'].values)
-    not_back = list(fail.groupby('info_yyyy').sum()['미복학'].values)
-    drop_out = list(fail.groupby('info_yyyy').sum()['자퇴'].values)
+
+    partici = list(student.groupby('info_yyyy').sum()['지원자'].values)
+    max_enroll = list(student.groupby('info_yyyy').sum()['입학정원'].values)
+
+    drop_out_values = list(drop_out.groupby('info_yyyy').sum()['중도탈락'].values)
+    not_enroll = list(drop_out.groupby('info_yyyy').sum()['미등록'].values)
+    not_back = list(drop_out.groupby('info_yyyy').sum()['미복학'].values)
+    leave = list(drop_out.groupby('info_yyyy').sum()['자퇴'].values)
+
+    # occasional = list(enter_type.groupby('info_yyyy').sum()['수시'].values)
+    # regular = list(enter_type.groupby('info_yyyy').sum()['정시'].values)
+    # additional = list(enter_type.groupby('info_yyyy').sum()['추가'].values)
+    # final = list(enter_type.sum()['최종'].values)
+    
+    # print(occasional)
     
     # numpy 객체를 -> list 객체로 변환
     m_partici = list(map(lambda x:int(x),partici))
     m_max_enroll = list(map(lambda x:int(x),max_enroll))
-    m_fail = list(map(lambda x:int(x),fail_values))
+    m_drop_out_total = list(map(lambda x:int(x),drop_out_values))
     m_not_enroll = list(map(lambda x:int(x),not_enroll))
     m_not_back = list(map(lambda x:int(x),not_back))
-    m_drop_out = list(map(lambda x:int(x),drop_out))
+    m_drop_out = list(map(lambda x:int(x),leave))
 
-    return jsonify(result="success", year=year, partici=m_partici, max_enroll=m_max_enroll, fail=m_fail, not_enroll=m_not_enroll, not_back=m_not_back, drop_out=m_drop_out)
+    return jsonify(result="success", year=year, partici=m_partici, max_enroll=m_max_enroll, drop_out_total=m_drop_out_total, not_enroll=m_not_enroll, not_back=m_not_back, drop_out=m_drop_out)
